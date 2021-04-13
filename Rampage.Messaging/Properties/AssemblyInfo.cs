4af919f201acb90daf21e00eccc32c2396603a30
mr-rampage <readonly@che.wbac.ca>
2021-04-13 09:35:18 -0400

    :green_heart: Added artifact configuration

--- Rampage.Messaging/Properties/AssemblyInfo.cs
+++ Rampage.Messaging/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rampage.Messaging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple messaging framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("wbac")]
 [assembly: AssemblyProduct("Rampage.Messaging")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
